c0aa01faf22645c0352b906241164440bd81b5c1
SakuraMarble <1932989740@qq.com>
2025-10-04 17:20:14 +0800

    example D: 4 to 5

--- sh/example.sh
+++ sh/example.sh
@@ -4,63 +4,63 @@ mkdir build
 cd build
 cmake ..
 make 
-./bin/D-PrunedDP++ 2 ../../../data/ Twitch 3 4 0 10
+./bin/D-PrunedDP++ 2 ../../../data/ Twitch 3 5 0 10
 
 cd ../../../code/D-TrimCDP
 mkdir build
 cd build
 cmake ..
 make 
-./bin/D-TrimCDP 2 ../../../data/ Twitch 3 4 0 10
+./bin/D-TrimCDP 2 ../../../data/ Twitch 3 5 0 10
 
 cd ../../../code/D-TrimCDP-multi-core-CPU
 mkdir build
 cd build
 cmake ..
 make
-./bin/D-PrunedDP++ 2 ../../../data/  Twitch  3 4 0 10
+./bin/D-PrunedDP++ 2 ../../../data/  Twitch  3 5 0 10
 
 cd ../../../code/D-TrimCDP-WB
 mkdir build
 cd build
 cmake ..
 make 
-./bin/D-TrimCDP-WB 1 ../../../data/ Twitch 3 4 0 10
+./bin/D-TrimCDP-WB 1 ../../../data/ Twitch 3 5 0 10
 
 cd ../../../code/D-TrimCDP-WB-kernel_fusion-no_shared_memory_prefix_scan-global_memory_coalescing
 mkdir build
 cd build
 cmake ..
 make
-./bin/D-TrimCDP-WB 2 ../../../data/  Twitch  3 4 0 10
+./bin/D-TrimCDP-WB 2 ../../../data/  Twitch  3 5 0 10
 
 cd ../../../code/D-TrimCDP-WB-kernel_fusion-shared_memory_prefix_scan-global_memory_coalescing
 mkdir build
 cd build
 cmake ..
 make
-./bin/D-TrimCDP-WB 2 ../../../data/  Twitch  3 4 0 10
+./bin/D-TrimCDP-WB 2 ../../../data/  Twitch  3 5 0 10
 
 cd ../../../code/D-TrimCDP-WB-no_global_memory_coalescing
 mkdir build
 cd build
 cmake ..
 make
-./bin/D-TrimCDP-WB 2 ../../../data/  Twitch  3 4 0 10
+./bin/D-TrimCDP-WB 2 ../../../data/  Twitch  3 5 0 10
 
 cd ../../../code/D-TrimCDP-WB-no_kernel_fusion
 mkdir build
 cd build
 cmake ..
 make
-./bin/D-TrimCDP-WB 2 ../../../data/  Twitch  3 4 0 10
+./bin/D-TrimCDP-WB 2 ../../../data/  Twitch  3 5 0 10
 
 cd ../../../code/D-TrimCDP-WB-no_virtual_split
 mkdir build
 cd build
 cmake ..
 make
-./bin/D-TrimCDP-WB 2 ../../../data/  Twitch  3 4 0 10
+./bin/D-TrimCDP-WB 2 ../../../data/  Twitch  3 5 0 10
 
 cd ../../../code/PrunedDP++
 mkdir build
